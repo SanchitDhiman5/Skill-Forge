b82316b22d07866cd1fa2a516ae8f4d15cb28c8e
Sanchit <dhimansanchit5@gmail.com>
2025-03-08 11:32:45 +0530

    added navbar and scroll to top button

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 const header = document.querySelector("header");
-
+let menu = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+const a = document.querySelectorAll('a[href="#"]');
 window.addEventListener("scroll", function () {
   header.classList.toggle("sticky", this.window.scrollY > 50);
+});
+
+// Navbar Menu:
+menu.addEventListener("click", function (e) {
+  menu.classList.toggle("bx-x");
+  navbar.classList.toggle("open");
+  if (navbar.classList.contains("open")) {
+    document.body.style.overflow = "hidden";
+  } else {
+    document.body.style.overflow = "scroll";
+  }
+});
+
+// prevent anchor tag's default behaviour:
+document.body.addEventListener("click", function (e) {
+  if (e.target.tagName === "A" && e.target.id !== "up-arrow") {
+    e.preventDefault();
+  }
 });
