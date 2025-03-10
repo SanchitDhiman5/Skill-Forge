4239683ff10e5730f40d9233226abb1e7bc93bda
Sanchit <dhimansanchit5@gmail.com>
2025-03-10 23:59:18 +0530

    added Direct scroll to the section from navLinks <a>

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const header = document.querySelector("header");
 let menu = document.querySelector("#menu-icon");
 let navbar = document.querySelector(".navbar");
 const a = document.querySelectorAll('a[href="#"]');
+const navLinks = document.querySelectorAll("nav a");
 window.addEventListener("scroll", function () {
   header.classList.toggle("sticky", this.window.scrollY > 50);
 });
@@ -17,9 +18,18 @@ menu.addEventListener("click", function (e) {
   }
 });
 
-// prevent anchor tag's default behaviour:
-document.body.addEventListener("click", function (e) {
-  if (e.target.tagName === "A" && e.target.id !== "up-arrow") {
-    e.preventDefault();
-  }
+// Navbar Direct Link scroll:
+navLinks.forEach((link) => {
+  link.addEventListener("click", function (e) {
+    // e.preventDefault();
+    const targetId = this.getAttribute("href").substring(1);
+    const targetSection = document.getElementById(targetId);
+
+    if (targetSection) {
+      window.scrollTo({
+        top: targetSection.offsetTop,
+        behavior: "smooth",
+      });
+    }
+  });
 });
